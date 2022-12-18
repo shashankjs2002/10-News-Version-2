16faf7c08e24ff520f9ecf82e9dab84d8368677d
shashankjs2002 <shashankjs2002@gmail.com>
2022-12-18 23:42:52 +0530

    v1

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -22,7 +22,7 @@ const Navbar = () => {
         // console.log(search)
         setQuery(search.search)
         // context.router.push()
-        navigate(`/10-News/search/${search.search}`)
+        navigate(`/search/${search.search}`)
         setSearch({search: ""})
         
     }
@@ -44,14 +44,14 @@ const Navbar = () => {
                     <li className="nav-link">
                     <Link className={`nav-link ${(path==='/') ? 'active': ''}`} to="/">Home</Link>
                     </li>
-                    <li className={`nav-link ${(path==='/10-News/world') ? 'active': ''}`}><Link className="nav-link" to="/10-News/world">World</Link></li>
-                    <li className={`nav-link ${(path==='/10-News/nation') ? 'active': ''}`}><Link className="nav-link" to="/10-News/nation">Nation</Link></li>
-                    <li className={`nav-link ${(path==='/10-News/business') ? 'active': ''}`}><Link className="nav-link" to="/10-News/business">Business</Link></li>
-                    <li className={`nav-link ${(path==='/10-News/entertainment') ? 'active': ''}`}><Link className="nav-link" to="/10-News/entertainment">Entertainment</Link></li>
-                    <li className={`nav-link ${(path==='/10-News/health') ? 'active': ''}`}><Link className="nav-link" to="/10-News/health">Health</Link></li>
-                    <li className={`nav-link ${(path==='/10-News/science') ? 'active': ''}`}><Link className="nav-link" to="/10-News/science">Science</Link></li>
-                    <li className={`nav-link ${(path==='/10-News/sports') ? 'active': ''}`}><Link className="nav-link" to="/10-News/sports">Sports</Link></li>
-                    <li className={`nav-link ${(path==='/10-News/technology') ? 'active': ''}`}><Link className="nav-link" to="/10-News/technology">Technology</Link></li>
+                    <li className={`nav-link ${(path==='/world') ? 'active': ''}`}><Link className="nav-link" to="/world">World</Link></li>
+                    <li className={`nav-link ${(path==='/nation') ? 'active': ''}`}><Link className="nav-link" to="/nation">Nation</Link></li>
+                    <li className={`nav-link ${(path==='/business') ? 'active': ''}`}><Link className="nav-link" to="/business">Business</Link></li>
+                    <li className={`nav-link ${(path==='/entertainment') ? 'active': ''}`}><Link className="nav-link" to="/entertainment">Entertainment</Link></li>
+                    <li className={`nav-link ${(path==='/health') ? 'active': ''}`}><Link className="nav-link" to="/health">Health</Link></li>
+                    <li className={`nav-link ${(path==='/science') ? 'active': ''}`}><Link className="nav-link" to="/science">Science</Link></li>
+                    <li className={`nav-link ${(path==='/sports') ? 'active': ''}`}><Link className="nav-link" to="/sports">Sports</Link></li>
+                    <li className={`nav-link ${(path==='/technology') ? 'active': ''}`}><Link className="nav-link" to="/technology">Technology</Link></li>
                     
                     {/* {(query!==null) ? :} */}
                     {/* <li className="nav-item"><Link className="nav-link" to={`/search=${query}`}>Technology</Link></li> */}
